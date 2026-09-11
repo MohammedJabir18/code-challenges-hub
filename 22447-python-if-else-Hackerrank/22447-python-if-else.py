6d97d6d7b171803c29767682c86d2aca2f2001c1
Mohammed Jabir <jabirahmedz111@gmail.com>
2026-09-11 23:08:52 +0530

    Force committed solution - Time: O(1), Space: O(1)

---
+++ 22447-python-if-else-Hackerrank/22447-python-if-else.py
@@ -0,0 +1,22 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+    
+    if n % 2 != 0 or 6 <= n <= 20:
+        print("Weird")
+    else:
+        print("Not Weird")
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
