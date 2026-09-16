94aff242672fadf0feb134d94f7dd8824b557fd3
Mohammed Jabir <jabirahmedz111@gmail.com>
2026-09-16 23:42:11 +0530

    Force committed solution - Time: O(n), Space: O(n)

---
+++ 8026-print-function-Hackerrank/8026-print-function.py
@@ -0,0 +1,9 @@
+if __name__ == '__main__':
+    n = int(input())
+    
+    print(*range(1, n+1), sep="")
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
